208a0df1d5990a33a081de07479136ecbd9b58cc
Adem Efe Devrez <adevrez77@gmail.com>
2023-03-05 18:38:48 +0300

    Take CL argument and check the CL argument count

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,30 @@
-use std::io;
+use std::env;
+use std::process;
 
 fn main() {
-    loop {
-        //part where we decide the operation
-        println!("Which converter would you like to use?\n1: Fahrenheit to Celsius\n2: Celsius to Fahrenheit\nPlease type 1\\2");
-        let mut operator:String = String::new();
-        io::stdin()
-            .read_line(&mut operator)
-            .expect("Failed to read line!");
-        let operator_but_int: i32 = match operator.trim().parse() {
-            Ok(operator_but_int) => operator_but_int,
-            Err(_) => continue
-        };
-        //part where we read input
-        println!("Please enter the value:");
-        let mut entered_value:String = String::new();
-            io::stdin()
-                .read_line(&mut entered_value)
-                .expect("Failed to read your line!");
-        let entered_value_but_trimmed = entered_value.trim();
-        let converted_value: f32 = match entered_value.trim().parse() {
-            Ok(converted_value) => converted_value,
-            Err(_) => continue,
-        };
-        //it's calculatin time
-        if operator_but_int == 1 {
-            let converted_value: f32 = 5.0/9.0 * (converted_value - 32.0);
-            println!("{} Fahrenheit equals to {} Celsius!", entered_value_but_trimmed, converted_value);
-            break;
-        } else if operator_but_int == 2 {
-            let converted_value: f32 = converted_value * 9.0 / 5.0 + 32.0;
-            println!("{} Celsius equals to {} Fahrenheit!", entered_value_but_trimmed, converted_value);
-            break;
-        } else {
-            println!("Please enter a valid operation!");
-            continue;
-        }
+    let args: Vec<String> = env::args().collect();
+
+    let degree = input_checker(&args).unwrap_or_else( |err| {
+        eprintln!("Problem while taking input: {err}");
+        process::exit(1);
+    });
+
+    println!("{degree}");
+}
+
+fn input_checker(args: &[String]) -> Result<String, &'static str> {
+    if args.len() < 2 {
+        return Err("Not enough arguments!"); //Temporary
+    } else if args.len() > 2 {
+        return Err("Too many arguments!");
+    } else {
+        let degree = args[1].clone();
+        Ok(degree)
     }
 }
 
-//i am learning how to use git properly
+//1. Program CL argümanlarını alsın. Eğer yeterli CL argümanı yoksa 2. adıma geçsin, yeterli CL argümanı varsa 3. adıma geçsin, fazla argüman varsa hata versin. ++
+//2. Eğer CL'den sayı alınmadıysa uyarı geçsin ve yine de sayıyı sorsun.
+//3. Program argümanı parse'lasın. Eğer girilen sayı parse'lanamıyorsa paniklesin.
+//4. Parse'lanan sayının C->F ve F -> C değerleri yazdırılsın. Eğer FC veya CF envvarları kullanılmışsa yalnızca ilgili işlemi bastırsın.
+//Not: Sayı değeri ve CF/FC envvarı kullanılıp kullanılmaması bir struct'ta birleştirilebilir. Envvar değerinin CF veya FC olması enum olabilir.
